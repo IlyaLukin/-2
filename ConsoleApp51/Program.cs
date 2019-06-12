494d7ef0dfa6b138b3198812c0e8a645c3ebd7d7
Лукин Илья Павлович <iplukin@edu.hse.ru>
2019-06-12 20:32:04 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n;
+            string s;
+            string[] split;
+            Console.WriteLine("Money,money,moeny");
+            Console.WriteLine("\nВведите число");
+            s = Console.ReadLine();
+            split = s.Split(new char[] { ' ' });
+            long.TryParse(split[0], out n);
+            if (n % 2 == 1)
+            {
+                Console.WriteLine($"2 {n + 2}");
+            }
+            else
+            {
+                Console.WriteLine("0 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
